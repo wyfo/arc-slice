4282ef85c7ed3680f99d8457b9769838757837cc
Joseph Perez <joperez@hotmail.fr>
2025-03-08 14:05:19 +0100

    add criterion benchmarks

--- benches/criterion.rs
+++ benches/criterion.rs
@@ -1,3 +1,5 @@
+use std::hint::black_box;
+
 use arc_slice::ArcBytes;
 use bytes::Bytes;
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
@@ -54,5 +56,42 @@ fn clone_shared(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, empty, clone_vec, clone_static, clone_shared);
+fn subslice_and_split(c: &mut Criterion) {
+    let mut group = c.benchmark_group("subslice_and_split");
+    group.bench_function("arcslice", |b| {
+        b.iter(|| {
+            let mut bytes = <ArcBytes>::new(b"Hello world");
+            let a = black_box(&bytes).subslice(0..5);
+
+            assert_eq!(black_box(&a), b"Hello");
+
+            let b = black_box(&mut bytes).split_to(6);
+
+            assert_eq!(black_box(&bytes), b"world");
+            assert_eq!(black_box(&b), b"Hello ");
+        });
+    });
+    group.bench_function("bytes", |b| {
+        b.iter(|| {
+            let mut bytes = Bytes::from("Hello world");
+            let a = black_box(&bytes).slice(0..5);
+
+            assert_eq!(black_box(&a), "Hello");
+
+            let b = black_box(&mut bytes).split_to(6);
+
+            assert_eq!(black_box(&bytes), "world");
+            assert_eq!(black_box(&b), "Hello ");
+        });
+    });
+}
+
+criterion_group!(
+    benches,
+    empty,
+    clone_vec,
+    clone_static,
+    clone_shared,
+    subslice_and_split
+);
 criterion_main!(benches);
