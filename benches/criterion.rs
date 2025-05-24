80ee40b65b2372fe9f35b845d72cfc500b588949
Joseph Perez <joperez@hotmail.fr>
2025-05-24 23:39:58 +0200

    new -> from_slice

--- benches/criterion.rs
+++ benches/criterion.rs
@@ -2,61 +2,61 @@ use std::hint::black_box;
 
 use arc_slice::ArcBytes;
 use bytes::Bytes;
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 
-// fn empty(c: &mut Criterion) {
-//     let mut group = c.benchmark_group("empty");
-//     group.bench_function("arcslice", |b| {
-//         b.iter(<ArcBytes>::default);
-//     });
-//     group.bench_function("bytes", |b| {
-//         b.iter(Bytes::default);
-//     });
-// }
-//
-// fn clone_vec(c: &mut Criterion) {
-//     let mut group = c.benchmark_group("clone_vec");
-//     group.bench_function("arcslice", |b| {
-//         b.iter_batched(
-//             || <ArcBytes>::from(vec![0u8; 8]),
-//             |bytes| bytes.clone(),
-//             BatchSize::SmallInput,
-//         );
-//     });
-//     group.bench_function("bytes", |b| {
-//         b.iter_batched(
-//             || Bytes::from(vec![0u8; 8]),
-//             |bytes| bytes.clone(),
-//             BatchSize::SmallInput,
-//         );
-//     });
-// }
-//
-// fn clone_static(c: &mut Criterion) {
-//     let mut group = c.benchmark_group("clone_static");
-//     group.bench_function("arcslice", |b| {
-//         let bytes = <ArcBytes>::new_static(&[]);
-//         b.iter(|| bytes.clone());
-//     });
-//     group.bench_function("bytes", |b| {
-//         let bytes = Bytes::from_static(&[]);
-//         b.iter(|| bytes.clone());
-//     });
-// }
-//
-// fn clone_shared(c: &mut Criterion) {
-//     let mut group = c.benchmark_group("clone_shared");
-//     group.bench_function("arcslice", |b| {
-//         let bytes = <ArcBytes>::from(vec![0u8; 8]);
-//         let _ = bytes.clone();
-//         b.iter(|| bytes.clone());
-//     });
-//     group.bench_function("bytes", |b| {
-//         let bytes = Bytes::from(vec![0u8; 8]);
-//         let _ = bytes.clone();
-//         b.iter(|| bytes.clone());
-//     });
-// }
+fn empty(c: &mut Criterion) {
+    let mut group = c.benchmark_group("empty");
+    group.bench_function("arcslice", |b| {
+        b.iter(<ArcBytes>::default);
+    });
+    group.bench_function("bytes", |b| {
+        b.iter(Bytes::default);
+    });
+}
+
+fn clone_vec(c: &mut Criterion) {
+    let mut group = c.benchmark_group("clone_vec");
+    group.bench_function("arcslice", |b| {
+        b.iter_batched(
+            || <ArcBytes>::from(vec![0u8; 8]),
+            |bytes| bytes.clone(),
+            BatchSize::SmallInput,
+        );
+    });
+    group.bench_function("bytes", |b| {
+        b.iter_batched(
+            || Bytes::from(vec![0u8; 8]),
+            |bytes| bytes.clone(),
+            BatchSize::SmallInput,
+        );
+    });
+}
+
+fn clone_static(c: &mut Criterion) {
+    let mut group = c.benchmark_group("clone_static");
+    group.bench_function("arcslice", |b| {
+        let bytes = <ArcBytes>::from_static(&[]);
+        b.iter(|| bytes.clone());
+    });
+    group.bench_function("bytes", |b| {
+        let bytes = Bytes::from_static(&[]);
+        b.iter(|| bytes.clone());
+    });
+}
+
+fn clone_shared(c: &mut Criterion) {
+    let mut group = c.benchmark_group("clone_shared");
+    group.bench_function("arcslice", |b| {
+        let bytes = <ArcBytes>::from(vec![0u8; 8]);
+        let _ = bytes.clone();
+        b.iter(|| bytes.clone());
+    });
+    group.bench_function("bytes", |b| {
+        let bytes = Bytes::from(vec![0u8; 8]);
+        let _ = bytes.clone();
+        b.iter(|| bytes.clone());
+    });
+}
 
 fn subslice_and_split(c: &mut Criterion) {
     let mut group = c.benchmark_group("subslice_and_split");
@@ -119,10 +119,10 @@ fn subslice_and_split_black_box(c: &mut Criterion) {
 }
 criterion_group!(
     benches,
-    // empty,
-    // clone_vec,
-    // clone_static,
-    // clone_shared,
+    empty,
+    clone_vec,
+    clone_static,
+    clone_shared,
     subslice_and_split,
     subslice_and_split_black_box,
 );
