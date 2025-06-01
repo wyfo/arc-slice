7e70e0adaf46d87b910d48c395ec53847c90b937
Joseph Perez <joperez@hotmail.fr>
2025-06-01 13:30:28 +0200

    use memmap2 for README example

--- README.md
+++ README.md
@@ -20,37 +20,20 @@ assert_eq!(b, b"Hello ");
 
 Using `arc-slice` with shared memory:
 ```rust
-use arc_slice::{
-    buffer::{BorrowMetadata, Buffer},
-    ArcBytes,
+use std::{
+    fs::File,
+    path::{Path, PathBuf},
 };
-use shared_memory::{Shmem, ShmemConf};
 
-struct MyShmBuffer(Shmem);
-unsafe impl Send for MyShmBuffer {}
-unsafe impl Sync for MyShmBuffer {}
-impl Buffer<u8> for MyShmBuffer {
-    fn as_slice(&self) -> &[u8] {
-        unsafe { self.0.as_slice() }
-    }
-}
+use arc_slice::{buffer::AsRefBuffer, ArcBytes};
+use memmap2::Mmap;
 
-#[repr(transparent)]
-struct MyShmMetadata(Shmem);
-unsafe impl Sync for MyShmMetadata {}
-impl BorrowMetadata for MyShmBuffer {
-    type Metadata = MyShmMetadata;
-    fn borrow_metadata(&self) -> &Self::Metadata {
-        unsafe { core::mem::transmute(&self.0) }
-    }
-}
+let path = Path::new("README.md");
+let file = File::open(path)?;
+let mmap = unsafe { Mmap::map(&file)? };
+let bytes = <ArcBytes>::from_buffer_with_metadata(AsRefBuffer(mmap), path.to_owned());
 
-let shmem = ShmemConf::new().size(8).create().unwrap();
-let os_id = shmem.get_os_id().to_owned();
-
-let bytes = <ArcBytes>::with_borrowed_metadata(MyShmBuffer(shmem));
-let metadata = bytes.get_metadata::<MyShmMetadata>().unwrap();
-assert_eq!(metadata.0.get_os_id(), os_id);
+assert_eq!(bytes.metadata::<PathBuf>().unwrap(), path);
 ```
 
 ## *Disclaimer*
--- tests/readme.rs
+++ tests/readme.rs
@@ -15,36 +15,20 @@ fn readme_example() {
 
 #[cfg(feature = "default-layout-any-buffer")]
 #[test]
-fn readme_example_shm() {
-    use arc_slice::{
-        buffer::{BorrowMetadata, Buffer},
-        ArcBytes,
+fn readme_example_memmap() -> std::io::Result<()> {
+    use std::{
+        fs::File,
+        path::{Path, PathBuf},
     };
-    use shared_memory::{Shmem, ShmemConf};
 
-    struct MyShmBuffer(Shmem);
-    unsafe impl Send for MyShmBuffer {}
-    unsafe impl Sync for MyShmBuffer {}
-    impl Buffer<[u8]> for MyShmBuffer {
-        fn as_slice(&self) -> &[u8] {
-            unsafe { self.0.as_slice() }
-        }
-    }
+    use arc_slice::{buffer::AsRefBuffer, ArcBytes};
+    use memmap2::Mmap;
 
-    #[repr(transparent)]
-    struct MyShmMetadata(Shmem);
-    unsafe impl Sync for MyShmMetadata {}
-    impl BorrowMetadata for MyShmBuffer {
-        type Metadata = MyShmMetadata;
-        fn borrow_metadata(&self) -> &Self::Metadata {
-            unsafe { core::mem::transmute(&self.0) }
-        }
-    }
+    let path = Path::new("README.md");
+    let file = File::open(path)?;
+    let mmap = unsafe { Mmap::map(&file)? };
+    let bytes = <ArcBytes>::from_buffer_with_metadata(AsRefBuffer(mmap), path.to_owned());
 
-    let shmem = ShmemConf::new().size(8).create().unwrap();
-    let os_id = shmem.get_os_id().to_owned();
-
-    let bytes = <ArcBytes>::from_buffer_with_borrowed_metadata(MyShmBuffer(shmem));
-    let metadata = bytes.metadata::<MyShmMetadata>().unwrap();
-    assert_eq!(metadata.0.get_os_id(), os_id);
+    assert_eq!(bytes.metadata::<PathBuf>().unwrap(), path);
+    Ok(())
 }
